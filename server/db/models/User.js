da49cb9bb3975d82de5a925d471d90b408e74fd5
Alexandra Pini <alexandrampini@gmail.com>
2019-10-23 22:39:14 -0400

    seeded data and user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,17 +1,42 @@
-const { STRING } = require('sequelize');
+const { STRING, UUID, UUIDV4 } = require('sequelize');
 const db = require('../db.js');
 
 // TODO: Fill out this model.
 const User = db.define('user', {
-  uselessColumn: {
-    type: STRING,
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
   },
   userName: {
-    type: STRING
+    type: STRING,
+    validate: {
+      notEmpty: true
+    }
   },
   password: {
     type: STRING
   }
 });
 
-module.exports = User;
+const sync = async () => {
+  await db.sync({ force: true });
+
+
+  const userNames = [
+    {userName: 'shay', password: 'SHAY' },
+    {userName: 'zan', password: 'ZAN' },
+    {userName: 'tdubs', password: 'TDUBS' },
+    {userName: 'mclark', password: 'MCLARK' },
+    {userName: 'jdmann', password: 'JDMANN' }
+  ];
+
+  const [shay, zan, tdubs, jdmann, mclark] = await Promise.all(userNames.map( userName => User.create(userName)));
+}
+
+module.exports = {
+  sync,
+  models: {
+    User
+  }
+};
